58878c7988a571f888391d0b9fb9d1b9be37cd00
dvanic <d.vanichkina@gmail.com>
2019-09-04 15:59:40 +1000

    Adde wed

--- challenges.R
+++ challenges.R
@@ -1,1 +1,16 @@
 # This is the file for the challenges
+
+
+
+
+
+# 07 vector shapefile attributes -------------
+
+# Challenge 1 (RENUMBER LATER!)
+# Using the steps above, import the HARV_roads and HARVtower_UTM18N layers into R. Call the HARV_roads object lines_HARV and the HARVtower_UTM18N point_HARV.
+# Answer the following questions:
+#
+#1. What type of R spatial object is created when you import each layer?
+#2. What is the CRS and extent for each object?
+#3.Do the files contain points, lines, or polygons?
+#4. How many spatial objects are in each file?
